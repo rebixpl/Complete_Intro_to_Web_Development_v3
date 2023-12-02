43d034d7dce5fcf8f65ec8bdc7c83a06dc2c2f93
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-02 09:29:01 +0100

    quiz app update

--- ReactJS/quiz_app/my-quiz/src/Quiz.jsx
+++ ReactJS/quiz_app/my-quiz/src/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
+import { resultInitialState } from "./constants";
 
 const Quiz = ({ questions }) => {
   Quiz.propTypes = {
@@ -15,6 +16,8 @@ const Quiz = ({ questions }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answerIdx, setAnswerIdx] = useState(null);
   const [answer, setAnswer] = useState(null);
+  const [result, setResult] = useState(resultInitialState);
+  const [showResult, setShowResult] = useState(false);
 
   const { question, choices, correctAnswer } = questions[currentQuestion];
 
@@ -27,33 +30,78 @@ const Quiz = ({ questions }) => {
     }
   };
 
-  const onClickNext = () => {};
+  const onClickNext = () => {
+    setAnswerIdx(null);
+    setResult((prev) =>
+      answer
+        ? {
+            ...prev,
+            score: prev.score + 5, // score value is 5 for each correct answer
+            correctAnswers: prev.correctAnswers + 1,
+          }
+        : {
+            ...prev,
+            wrongAnswers: prev.wrongAnswers + 1,
+          }
+    );
+
+    if (currentQuestion !== questions.length - 1) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setCurrentQuestion(0); // reached the end
+      setShowResult(true);
+    }
+  };
+
+  const onTryAgain = () => {
+    setResult(resultInitialState);
+    setShowResult(false);
+  };
 
   return (
     <div className="quiz-container">
-      <>
-        <span className="active-question-no">{currentQuestion + 1}</span>
-        <span className="total-question">/{questions.length}</span>
-        <h2>{question}</h2>
-        <ul>
-          {choices.map((answer, index) => (
-            <li
-              onClick={() => {
-                onAnswerClick(answer, index);
-              }}
-              key={index}
-              className={answerIdx === index ? "selected-answer" : null}
-            >
-              {answer}
-            </li>
-          ))}
-        </ul>
-        <div className="footer">
-          <button onClick={onClickNext} disabled={answerIdx == null}>
-            {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
-          </button>
+      {!showResult ? (
+        <>
+          <span className="active-question-no">{currentQuestion + 1}</span>
+          <span className="total-question">/{questions.length}</span>
+          <h2>{question}</h2>
+          <ul>
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => {
+                  onAnswerClick(answer, index);
+                }}
+                key={index}
+                className={answerIdx === index ? "selected-answer" : null}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className="footer">
+            <button onClick={onClickNext} disabled={answerIdx == null}>
+              {currentQuestion === questions.length - 1 ? "Finish" : "Next"}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="result">
+          <h3>Result</h3>
+          <p>
+            Total Questions: <span> {questions.length}</span>
+          </p>
+          <p>
+            Total Score: <span> {result.score}</span>
+          </p>
+          <p>
+            Correct Answers: <span> {result.correctAnswers}</span>
+          </p>
+          <p>
+            Wrong Answers: <span> {result.wrongAnswers}</span>
+          </p>
+          <button onClick={onTryAgain}>Try again</button>
         </div>
-      </>
+      )}
     </div>
   );
 };
