aa1f28d6f77834fa203e1271a919758475738622
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-10 14:36:09 +0100

    implemented cloudinary get all posts

--- ReactJS/dalle_clone/server/routes/postRoutes.js
+++ ReactJS/dalle_clone/server/routes/postRoutes.js
@@ -15,7 +15,15 @@ cloudinary.config({
 });
 
 // GET ALL POSTS
-router.route("/").get(async (req, res) => {});
+router.route("/").get(async (req, res) => {
+  try {
+    const posts = await Post.find({});
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
+});
 
 // CREATE A POST
 router.route("/").post(async (req, res) => {
