53c9514f9628f5141851851c06bb969e719191bc
rebixpl <33410494+rebixpl@users.noreply.github.com>
2023-12-10 13:48:41 +0100

    instead of openai api i've integrated it with my local stablediffusion installation. It's free and even better then paid API

--- ReactJS/dalle_clone/server/routes/dalleRoutes.js
+++ ReactJS/dalle_clone/server/routes/dalleRoutes.js
@@ -1,21 +1,12 @@
 import express from "express";
 import * as dotenv from "dotenv";
-// import { Configuration, OpenAIApi } from "openai";
-import { OpenAI } from "openai";
+import axios from "axios";
 
 import Post from "../mongodb/models/post.js";
 
 dotenv.config();
 
 const router = express.Router();
-
-// const configuration = new Configuration({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
-
-// const openai = new OpenAIApi(configuration);
-
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 router.route("/").get((req, res) => {
   res.send("Hello from DALL-E 4.0 API");
@@ -25,20 +16,45 @@ router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
-      model: "dall-e-3",
+    const url = "http://192.168.0.119:7860/sdapi/v1/txt2img";
+
+    const negative_prompt =
+      "paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), skin spots, acnes, skin blemishes, age spot, glans, lowres, bad anatomy, bad hands, text, error, missing fingers,extra digit, fewer digits, cropped, worstquality, low quality, normal quality,jpegartifacts,signature, watermark, username,blurry,bad feet,cropped,poorly drawn hands,poorly drawn face,mutation,deformed,worst quality,low quality,normal quality,jpeg artifacts,signature,watermark,extra fingers,fewer digits,extra limbs,extra arms,extra legs,malformed limbs,fused fingers,too many fingers,long neck,cross-eyed,mutated hands,polar lowres,bad body,bad proportions,gross proportions,text,error,missing fingers,missing arms,missing legs,extra digit,penis,man,cock,(ng_deepnegative_v1_75t),(negative_hand-neg)";
+
+    const sd_model_checkpoint =
+      "epicrealism_pureEvolutionV35.safetensors [ad5d4d0031]";
+
+    const sdPayload = {
       prompt,
-      n: 1,
-      size: "1024x1024",
-      response_format: "b64_json",
-    });
+      negative_prompt,
+      sd_model_checkpoint,
+      seed: -1,
+      steps: 20,
+      n_iter: 1,
+      restore_faces: false,
+      hr_scale: 2,
+      enable_hr: true,
+      hr_upscaler: "R-ESRGAN 4x+",
+      denoising_strength: 0.7,
+      sampler_index: "UniPC",
+      sampler_name: "UniPC",
+      width: 512,
+      height: 512,
+      cfg_scale: 7.5,
+    };
+    const aiResponse = await axios.post(url, sdPayload);
 
-    const image = response.data.data[0].b64_json;
-
-    res.status(200).json({ photo: image });
+    if (aiResponse.data) {
+      // Assuming the response contains the image in base64 inside a json structure
+      res.status(200).json({ photo: aiResponse.data.images[0] });
+    } else {
+      res
+        .status(500)
+        .json({ message: "No data received from the image generation API" });
+    }
   } catch (error) {
     console.log(error);
-    res.status(500).send(error?.response.data.error.message);
+    res.status(500).send(error);
   }
 });
 
